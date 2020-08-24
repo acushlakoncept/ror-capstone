fd17de9bb1df9302dba0cfc0ba34591cfb369969
Uduak Essien <acushla4real@gmail.com>
2020-08-24 10:51:27 +0100

    add bootstrap gem and configure using webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['Popper.js', 'default']
+  })
+)
+
 module.exports = environment
