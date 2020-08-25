264d416dadc81593aedd414b4125b54f0bcd4b81
Uduak Essien <acushla4real@gmail.com>
2020-08-25 14:23:10 +0100

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :username, presence: true, length: { minimum: 3, maximum: 10 }
+    validates :fullname, presence: true, length: { minimum: 3, maximum: 20 }
 end
