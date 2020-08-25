134cc9223b509e78f67ba11a65285cf24e27d589
Uduak Essien <acushla4real@gmail.com>
2020-08-25 16:04:30 +0100

    add sessions controller methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    def new; end
+  
+    def create
+      @user = User.find_by(username: params[:session][:user])
+  
+      if @user
+        session[:username] = @user.username
+        redirect_to root_path
+      else
+        flash.now[:danger] = 'Something wrong with the login information'
+        render 'new'
+      end
+    end
+  
+    def destroy
+      session[:username] = nil
+      flash[:success] = 'You have successfully logged out'
+      redirect_to root_path
+    end
+  end
+  
