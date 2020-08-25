b10679c3ec6561e9b33ad0efdcc5e74e5ba85fbf
Uduak Essien <acushla4real@gmail.com>
2020-08-25 15:58:46 +0100

    add create user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,31 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+    #TODO: redirect to root path if logged in
   end
 
   def create
+    @user = User.new(sign_up_params)
+    if @user.save
+      flash[:notice] = 'Account was created successfully'
+      session[:username] = @user.username
+      redirect_to root_path
+    else
+      flash[:alert] = 'Something went wrong'
+      render 'new'
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:username, :fullname)
   end
 end
