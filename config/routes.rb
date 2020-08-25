6b88039e15bb84822ea79d2997a19218b18839af
Uduak Essien <acushla4real@gmail.com>
2020-08-25 16:31:30 +0100

    create sign page, update session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :users
+  resources :users, only: %i[new create show]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
