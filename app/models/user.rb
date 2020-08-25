f82f57a2dba27719a7317729e2ab20319e9b634c
Uduak Essien <acushla4real@gmail.com>
2020-08-25 15:43:02 +0100

    add uniquesness validation and test for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+    validates_uniqueness_of :username
     validates :username, presence: true, length: { minimum: 3, maximum: 10,
                                                    too_long: 'Maximum allowed username is 10 characters.',
                                                    too_short: 'Minimum allowed characters for username is 3' }
     validates :fullname, presence: true, length: { minimum: 6, maximum: 20,
                                                    too_long: 'Maximum allowed fullname is 20 characters.',
-                                                   too_short: 'Minimum allowed characters for fullname is 6'}
+                                                   too_short: 'Minimum allowed characters for fullname is 6'}                                             
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should validate_uniqueness_of(:username) }
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:fullname) }
 
