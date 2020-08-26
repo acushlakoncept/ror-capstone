31179f130661db17e58ce55eac817124232dcec6
Uduak Essien <acushla4real@gmail.com>
2020-08-26 17:09:38 +0100

    add ability for users to add tweet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
     before_action :require_user
     def index
         @opinion = Opinion.new
-        @opinions = Opinion.all
+        @opinions = Opinion.ordered_by_most_recent
     end
 end
--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,4 +1,6 @@
 class Opinion < ApplicationRecord
     validates_presence_of :text
     belongs_to :user
+
+    scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 end
