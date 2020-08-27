45f1212a40110bc4d1a5025c919b48511b1b9002
Uduak Essien <acushla4real@gmail.com>
2020-08-27 15:20:43 +0100

    add display_follow_count

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -23,4 +23,13 @@ module UsersHelper
      
        render partial: the_partial, locals: {obj: user}
     end
+
+    def display_follower_count(obj)
+      return 'No Followers'.html_safe unless obj.followers.count > 0
+
+      out = ''
+      out << 'Followed by ' 
+      out << link_to(obj.followers.first.fullname, profile_path(obj.followers.first.username))
+      out.html_safe
+    end
 end
