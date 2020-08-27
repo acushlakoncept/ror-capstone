f186746fee03d731d59f62591fa43a1508350e46
Uduak Essien <acushla4real@gmail.com>
2020-08-27 20:01:22 +0100

    add integration test to follow a user

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -21,3 +21,32 @@ describe 'Authentication', type: :feature do
   end
 
 end
+
+describe 'Authenticated User Activities', type: :feature do
+    before :each do
+        @user1 = User.create(username: 'acushla', fullname: 'Uduak Essien')
+        @user2 = User.create(username: 'faith', fullname: 'Faith Essien')
+        visit '/login'
+        fill_in 'session_username', with: 'acushla'
+        click_button 'Login'
+    end
+
+    it 'visits user profile page' do
+        visit profile_path(@user1.username)
+        expect(page).to have_current_path('/users/acushla')
+        expect(page).to have_text('TWEET TO UDUAK ESSIEN')
+    end
+
+    it 'accesses another user path' do
+        visit '/users/faith'
+        expect(page).to have_http_status(:success)
+        expect(page).to have_text('TWEET TO FAITH ESSIEN')
+    end
+
+    it 'follows a user' do
+        visit '/users/faith'
+        expect(page).to have_text('0 Following')
+        find('a.follow_me').click
+        expect(page).to have_text('1 Following')
+    end
+end
