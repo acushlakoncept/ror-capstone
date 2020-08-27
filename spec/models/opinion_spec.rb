10252f7d38f971b9561ff3ee9fcd297fbdf4b5e1
Uduak Essien <acushla4real@gmail.com>
2020-08-27 17:33:29 +0100

    add association test for opinion

--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Opinion, type: :model do
     it { should validate_length_of(:text).is_at_most(180) }
     it { should_not validate_length_of(:text).is_at_least(2) }
   end
+
+  describe 'Associations' do
+    it { should belong_to(:author) }
+  end
 end
