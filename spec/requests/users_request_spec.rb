05f25ab6a8357f92e3bc38342d9e7e84db5f4619
Uduak Essien <acushla4real@gmail.com>
2020-08-27 17:49:22 +0100

    add user controller/request test

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/users/new'
-      expect(response).to have_http_status(:success)
-    end
+  
+  describe 'GET #index' do
+    before { get root_path }
+
+    it { should redirect_to(login_path) }
   end
 
-  describe 'GET /login' do
+  describe 'GET #login' do
+    before { get login_path }
+ 
     it 'returns http success' do
-      get '/login'
       expect(response).to have_http_status(:success)
     end
+    it { should render_template('new') }
   end
 end
