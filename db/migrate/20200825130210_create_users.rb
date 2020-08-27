da8b1433a6244fb8315cdf74eae32255c76da0ca
Uduak Essien <acushla4real@gmail.com>
2020-08-27 13:20:01 +0100

    add user stats on profile page

--- db/migrate/20200825130210_create_users.rb
+++ db/migrate/20200825130210_create_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username, index: true
       t.string :fullname
-      t.string :photo, default: "https://source.unsplash.com/random/60x60"
-      t.string :cover_image, default: "https://source.unsplash.com/random/600x250"
+      t.string :photo, default: "https://source.unsplash.com/random/150x150"
+      t.string :cover_image, default: "https://source.unsplash.com/random/800x250"
 
       t.timestamps
     end
