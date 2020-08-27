bf234843f27715f6888701e250a184a6790bb1d9
Uduak Essien <acushla4real@gmail.com>
2020-08-27 18:37:11 +0100

    add test case for case insensitivity

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -12,11 +12,12 @@ describe 'Authentication', type: :feature do
     expect(current_path).to eq(root_path)
   end
 
-  it 'does not sign in with wrong info' do
-    visit '/users/sign_in'
-    fill_in 'Email', with: 'user1@gmail.com'
-    fill_in 'Password', with: 'mypassword'
-    click_button 'Log in'
-    expect(page).to have_text('Invalid Email or password.')
+  it 'allows case insensitivity for authentication' do
+    visit login_path
+    fill_in 'session_username', with: 'ACUSHLA'
+    click_button 'Login'
+    expect(current_path).to eq(root_path)
+    expect(page).to have_http_status(200)
   end
+
 end
