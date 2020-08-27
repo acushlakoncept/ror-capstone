bd83bbfa01e8b970a114941c409abbcecfe3cc25
Uduak Essien <acushla4real@gmail.com>
2020-08-27 15:26:14 +0100

    add helper method to set_active_class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def set_active_class(url_action)
+        'active' if params[:action] == url_action
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,22 +1,4 @@
 module UsersHelper
-    def follow_btn(obj)
-        return unless current_user.id != obj.id
-    
-        return if current_user.friend?(obj)
-    
-        out = ''
-        if current_user.pending_friends.include?(obj)
-          out << link_to('pending friendship', '#')
-        elsif current_user.friend_requests.include?(obj)
-          out << link_to('Accept', invite_path(user_id: obj.id), method: :put)
-          out << ' | '
-          out << link_to('Reject', reject_path(user_id: obj.id), method: :delete)
-        else
-          out << link_to('Invite to friendship', invite_path(user_id: obj.id), method: :post)
-        end
-    
-        out.html_safe
-      end
 
     def display_follow_btn(user, the_partial)
        return if current_user == user || current_user.follows.include?(user)
