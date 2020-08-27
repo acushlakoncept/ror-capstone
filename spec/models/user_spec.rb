56990bb31edce6ce835e647f546aa8368004d715
Uduak Essien <acushla4real@gmail.com>
2020-08-27 17:24:02 +0100

    add user association specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe User, type: :model do
       should validate_presence_of(:username)
         .with_message('Username cannot be blank')
     end
+    it do
+      should validate_length_of(:username)
+        .is_at_most(10)
+        .with_message('Maximum allowed username is 10 characters.')
+    end
     it { should_not validate_length_of(:username).is_at_least(2) }
     it { should_not validate_length_of(:username).is_at_most(11) }
     it { should validate_uniqueness_of(:username).case_insensitive.with_message("Username already taken.")}
@@ -21,41 +26,20 @@ RSpec.describe User, type: :model do
         .is_at_most(20)
         .with_message('Maximum allowed fullname is 20 characters.')
     end
+
+    it do
+      should_not validate_length_of(:fullname)
+        .is_at_least(5)
+        .with_message('Minimum allowed characters for fullname is 6')
+    end
   end
 
-  describe 'Model Associations' do
-    it { should have_many(:expenses).with_foreign_key(:author_id) }
-    it { should have_many(:groups).with_foreign_key(:author_id) }
-    it { should have_many(:expenses).dependent(:destroy) }
+  describe 'User Associations' do
+    it { should have_many(:opinions).with_foreign_key(:author_id) }
+    it { should have_many(:followings).with_foreign_key(:follower_id) }
+    it { should have_many(:follows).through(:followings) }
+    it { should have_many(:followers).through(:inverse_followings) }
   end
 
-  it do
-    should validate_uniqueness_of(:username)
-      .with_message('Username already taken.')
-  end
-
-  it do
-    should validate_length_of(:username)
-      .is_at_most(10)
-      .with_message('Maximum allowed username is 10 characters.')
-  end
-
-  it do
-    should validate_length_of(:fullname)
-      .is_at_most(20)
-      .with_message('Maximum allowed fullname is 20 characters.')
-  end
-
-  it do
-    should_not validate_length_of(:username)
-      .is_at_least(2)
-      .with_message('Minimum allowed characters for username is 3')
-  end
-
-  it do
-    should_not validate_length_of(:fullname)
-      .is_at_least(5)
-      .with_message('Minimum allowed characters for fullname is 6')
-  end
 end
 
