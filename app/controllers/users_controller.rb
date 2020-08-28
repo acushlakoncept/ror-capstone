9a4bc7e1d98d1ea7e005fbb758b883a28369af40
Uduak Essien <acushla4real@gmail.com>
2020-08-28 15:42:09 +0100

    update user profile style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user
-  
+  before_action :require_user, except: [:new, :create]
+
   def new
     @user = User.new
     redirect_to root_path if logged_in?
