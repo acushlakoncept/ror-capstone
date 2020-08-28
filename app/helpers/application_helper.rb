8f379dc4fca594190361a9e28661e8d2acbefc73
Uduak Essien <acushla4real@gmail.com>
2020-08-28 12:37:25 +0100

    fix rubocop lint errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,18 +5,18 @@ module ApplicationHelper
 
   def show_cover_image(user)
     if user.cover_image.present?
-      image_tag("#{user.cover_image}", alt: user.username, class: 'pr-2') 
+      image_tag(user.cover_image.to_s, alt: user.username, class: 'pr-2')
     else
-      image_tag('https://source.unsplash.com/random/800x250', alt: user.username, class: 'pr-2') 
+      image_tag('https://source.unsplash.com/random/800x250', alt: user.username, class: 'pr-2')
     end
   end
 
-  def show_photo(user, extra_class='')
+  def show_photo(user, extra_class = '')
     if user.photo.present?
-      image_tag("#{user.photo}", alt: user.username, class: "pr-2 rounded-circle profile-img #{extra_class}") 
+      image_tag(user.photo.to_s, alt: user.username, class: "pr-2 rounded-circle profile-img #{extra_class}")
     else
-      image_tag('https://source.unsplash.com/random/60x60', alt: user.username, class: 'pr-2 rounded-circle profile-img') 
+      image_tag('https://source.unsplash.com/random/60x60',
+                alt: user.username, class: 'pr-2 rounded-circle profile-img')
     end
   end
-
 end
