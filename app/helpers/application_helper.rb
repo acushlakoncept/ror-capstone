fd72ef86360f027f4ecd4e95243e42fd49db4124
Uduak Essien <acushla4real@gmail.com>
2020-08-28 11:05:29 +0100

    add helper method to show cover image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def active_class(url_action)
     'active' if params[:action] == url_action
   end
+
+  def show_cover_image(user)
+    if user.cover_image.present?
+      image_tag("#{user.cover_image}", alt: user.username, class: 'pr-2') 
+    else
+      image_tag('https://source.unsplash.com/random/800x250', alt: user.username, class: 'pr-2') 
+    end
+  end
 end
+
+
