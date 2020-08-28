8d94cba9f8811daa11b680c436b8e75fd323cab8
Uduak Essien <acushla4real@gmail.com>
2020-08-28 12:11:21 +0100

    update dependent destroy method for followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :opinions, foreign_key: 'author_id', dependent: :destroy
-  has_many :followings, foreign_key: 'follower_id'
+  has_many :followings, foreign_key: 'follower_id', dependent: :destroy
   has_many :follows, through: :followings, source: :followed
   has_many :inverse_followings, class_name: 'Following', foreign_key: 'followed_id'
   has_many :followers, through: :inverse_followings
