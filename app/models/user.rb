8361a809dd78baeac1ee4b8e31a06c3c3364a1a9
Uduak Essien <acushla4real@gmail.com>
2020-08-28 11:49:02 +0100

    update user model to destroy opinions on user delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :opinions, foreign_key: 'author_id'
+  has_many :opinions, foreign_key: 'author_id', dependent: :destroy
   has_many :followings, foreign_key: 'follower_id'
   has_many :follows, through: :followings, source: :followed
   has_many :inverse_followings, class_name: 'Following', foreign_key: 'followed_id'
