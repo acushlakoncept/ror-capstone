9eef917901be20e0145e4d2fa46777912dd92aad
Uduak Essien <acushla4real@gmail.com>
2020-08-30 05:16:59 +0100

    add association specs for likes

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:opinion) }
+  end
 end
